81267246beb0f648ca6ac378c6cedf035f2c18dc
Etty Werczberger <ewerczbe@student.touro.edu>
2025-11-25 17:26:07 -0500

    did the class assignment

--- src/main/java/exercises/shopping_app/ShoppingItem.java
+++ src/main/java/exercises/shopping_app/ShoppingItem.java
@@ -12,5 +12,34 @@ package exercises.shopping_app;
  * Name (alphabetically) when aisles are equal
  */
 public class ShoppingItem implements Comparable<ShoppingItem> {
-    //TODO
+    String aisle;
+    String name;
+
+    public ShoppingItem(String aisle, String name) {
+        this.aisle = aisle;
+        this.name = name;
+    }
+
+    public String getAisle() {
+        return aisle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("ShoppingItem: " + "aisle- " + this.aisle + " name- " + this.name );
+        return sb.toString();
+    }
+    @Override
+    public int compareTo(ShoppingItem other) {
+        int aisleComparison = this.aisle.compareTo(other.aisle);
+        if (aisleComparison != 0) {
+            return aisleComparison;
+        }
+        return this.name.compareTo(other.name);
+    }
+
 }
